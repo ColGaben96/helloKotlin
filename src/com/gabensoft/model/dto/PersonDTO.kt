0d450e061f82dde931e9350b54462d15886b0a25
Gabriel Blanco <geblarot96@gmail.com>
2021-09-22 11:26:28 -0500

    Added Organized Search

--- src/com/gabensoft/model/dto/PersonDTO.kt
+++ src/com/gabensoft/model/dto/PersonDTO.kt
@@ -2,4 +2,8 @@ package com.gabensoft.model.dto
 
 import java.io.Serializable
 
-data class PersonDTO (var id: Int, var nombre: String, var apellido: String, var email: String?) : Serializable
+data class PersonDTO (var id: Int, var nombre: String, var apellido: String, var email: String?) : Serializable, Comparator<PersonDTO> {
+    override fun compare(o1: PersonDTO?, o2: PersonDTO?): Int {
+        return o1!!.apellido.compareTo(o2!!.apellido)
+    }
+}
